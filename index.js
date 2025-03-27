59ed755ab700fea63508d8412e1f89487fe9b1bd
Mercy <owinomercy983@gmail.com>
2025-03-27 10:35:29 +0300

    added a joke add button and random joke button and a getelementbyid

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded" ,function(){
-    
+    const input=document.getElementById("Joke-genre")
+    const searchButton=document.getElementById("button")
+    const display = document.getElementById("display");
+    const addJokeBtn = document.getElementById("add-joke-btn");
+    const newCategory = document.getElementById("new-category");
+    const newSetup = document.getElementById("new-setup");
+    const newPunchline = document.getElementById("new-punchline");
+    const randomJokeBtn = document.getElementById("random-joke-btn");
 })
